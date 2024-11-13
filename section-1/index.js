c34610c797be2b3ff7018ba44334c6d79696c162
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-11-12 23:02:47 -0500

    update

--- section-1/index.js
+++ section-1/index.js
@@ -2,3 +2,11 @@ const fs = require("fs"); //file system
 
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
+
+const textOut = `this is what we know about the avocado ${textIn}. \nCreated on ${Date.now().toLocaleString()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log(`File has been written out`);
+
+// Blocking and Non-Blocking: Asynchronous
+// Synchronous code is also called blocking code
+// Asynchronous code is also called non-blocking code
