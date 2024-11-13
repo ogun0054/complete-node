c92fa366e0c7b1b75ea4c0d3f1a148a16628dd4c
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-11-13 14:07:01 -0500

    rounting example

--- section-1/index.js
+++ section-1/index.js
@@ -1,4 +1,9 @@
 const fs = require("fs"); //file system
+const http = require("http");
+const url = require("url");
+
+///////////////////////////
+// File
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -13,22 +18,52 @@ const fs = require("fs"); //file system
 // Asynchronous code is also called non-blocking code
 
 // Non-blocking, Asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  err
-    ? console.log("Error!")
-    : fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-        console.log(data2);
-        fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-          console.log(data3);
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   err
+//     ? console.log("Error!")
+//     : fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//         console.log(data2);
+//         fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//           console.log(data3);
 
-          fs.writeFile(
-            "./txt/final.txt",
-            `${data2}\n${data3}`,
-            "utf-8",
-            (err) => {
-              console.log("file has been written");
-            }
-          );
-        });
-      });
+//           fs.writeFile(
+//             "./txt/final.txt",
+//             `${data2}\n${data3}`,
+//             "utf-8",
+//             (err) => {
+//               console.log("file has been written");
+//             }
+//           );
+//         });
+//       });
+// });
+
+///////////////////////////
+// Server
+const server = http.createServer((req, res) => {
+  //   console.log(req);
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW!");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+    });
+    res.end("<h1>This page can't be found</h1>");
+  }
+
+  //   console.log(req.url);
+  //   res.end("Hello from the server!");
 });
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
+
+// Routing : Implementing different acting for different URL
+
+// 404 error: http status code
+// http header
