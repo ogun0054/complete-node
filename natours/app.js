9c51c29ecac9abcaed19955a5e70995d9b77bba6
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-12-09 11:38:27 -0500

    update

--- natours/app.js
+++ natours/app.js
@@ -31,12 +31,23 @@ app.get('/api/v1/tours', (req, res) => {
 
 app.get('/api/v1/tours/:id', (req, res) => {
   console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
   res.status(200).json({
     status: 'success',
-    // result: tours.length,
-    // data: {
-    //   tours,
-    // },
+    result: tours.length,
+    data: {
+      tour,
+    },
   });
 });
 
@@ -62,6 +73,22 @@ app.post('/api/v1/tours', (req, res) => {
   );
 
   // res.send('Done');
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here....>',
+    },
+  });
 });
 
 const port = 3000;
