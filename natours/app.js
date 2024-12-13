9ce00fcaa0e39534a8a14f2aa6df6a48c0eb5c73
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-12-12 23:29:44 -0500

    update

--- natours/app.js
+++ natours/app.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello from the Middleware');
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
