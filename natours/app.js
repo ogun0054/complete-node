3de1e3cae9a22f71acaec40daea63f775aac84f2
Ismail Adeshola Ogundele <ismailadesholaharun@gmail.com>
2024-12-16 22:01:44 -0500

    update

--- natours/app.js
+++ natours/app.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) Middlewares
+app.use(morgan('dev'));
 
 app.use(express.json());
 
@@ -18,6 +22,8 @@ app.use((req, res, next) => {
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 2) Route Handlers
 
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -111,6 +117,8 @@ const updateTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) Routes
+
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app
   .route('/api/v1/tours/:id')
@@ -118,6 +126,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+// 4) Start Server
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
